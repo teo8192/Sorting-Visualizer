1abfb718866d8a57ee120142095c164ebd548fad
Teo <teodor@dahlknutsen.no>
2019-05-23 14:00:41 +0200

    Better import and docstrings

--- c_sort.py
+++ c_sort.py
@@ -3,36 +3,35 @@
 Visualize C functions
 Author: Teodor Dahl Knutsen <teodor@dahlknutsen.no>
 """
-from ctypes import *
-from array import array
+from ctypes import CDLL, POINTER, c_int, CFUNCTYPE
 from visualizer import Visualize
 
 class SortC(Visualize):
+    """
+    Make the Visualize calls able to visualize c algorithms
+    """
     def __init__(self, dim=1000, mode="rainbow", block_size=None, num=None):
         super().__init__(dim, mode, block_size, num)
         self.libsort = CDLL("./libsort.so")
-        self.sortfunction = None
+        self.libsort.sort.restype = None
+        self.libsort.sort.argtypes = [POINTER(c_int),
+                                      c_int,
+                                      CFUNCTYPE(c_int, POINTER(c_int))]
 
     def sortfun(self):
+        """
+        Returns a compatible python function
+        that is wrapping the c function
+        """
         def fun(data, drawfn):
             def draw(dat):
                 self.data = list(dat[0:(len(data))])
                 drawfn()
-                return 0
+                return len(self.data)
 
-            drawfun = CFUNCTYPE(c_int, (POINTER(c_int)))(draw)
-            dataarr = (c_int * len(data))(*data)
-
-            self.libsort.sort.restype = None
-            self.libsort.sort.argtypes = [POINTER(c_int),
-                                          c_int,
-                                          CFUNCTYPE(c_int, POINTER(c_int))]
-
-            self.libsort.sort(dataarr, len(data), drawfun)
-
-            # sortfunc(dataarr,
-                     # len(data),
-                     # drawfun)
+            self.libsort.sort((c_int * len(data))(*data),               # The array
+                              len(data),                                # length of the array
+                              CFUNCTYPE(c_int, (POINTER(c_int)))(draw)) # Function pointer
 
         return fun
 
@@ -45,4 +44,4 @@ class SortC(Visualize):
         self.visualize(self.sortfun())
 
 if __name__ == '__main__':
-    SortC(mode="boxes").run()
+    SortC(mode="grayscale", block_size=4).run()
